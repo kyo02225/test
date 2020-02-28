c8650d55c12aa0c52ebc1573a5747e53df23dbe4
kyo02225 <quon.kyo.suica02225@gmail.com>
2020-02-28 17:17:45 +0900

    PostRequestを作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
@@ -22,11 +23,7 @@ class PostsController extends Controller
         return view('posts.create');
     }
 
-    public function store(Request $request) {
-        $this->validate($request, [
-            'title' => 'required|min:3',
-            'body' => 'required'
-        ]);
+    public function store(PostRequest $request) {
         $post = new Post();
         $post->title = $request->title;
         $post->body = $request->body;
@@ -37,4 +34,12 @@ class PostsController extends Controller
     public function edit(Post $post) {
         return view('posts.edit')->with('post', $post);
     }
+
+    public function update(PostRequest $request, Post $post) {
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('/');
+    }
+
 }
