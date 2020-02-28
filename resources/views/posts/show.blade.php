98deacdefa94a8a91c626e8bc87917c27d123bcf
kyo02225 <quon.kyo.suica02225@gmail.com>
2020-02-28 16:20:50 +0900

    create機能の実装

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,9 @@
 @section('title', $post->title)
 
 @section('content')
+<h1>
   <a href="{{ url('/') }}" class = "header-menu">Back</a>
-  <h1>{{ $post->title }}</h1>
-  <p>{!! nl2br(e($post->body)) !!}</p>
+  {{ $post->title }}
+</h1>
+<p>{!! nl2br(e($post->body)) !!}</p>
 @endsection
